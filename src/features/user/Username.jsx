0ead9ecb5969ca0bb9ca2eaf077f7ff70b2c229b
felipeSilv4Dev <felipesilva.developer@gmail.com>
2025-06-05 10:41:16 -0300

    styling home , cart-overview and applayout

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <p className="hidden text-sm font-semibold md:block">Jonas</p>;
+}
+
+export default Username;
--- src/user/Username.jsx
+++
@@ -1,5 +0,0 @@
-function Username() {
-  return <p className="text-sm font-semibold">Jonas</p>;
-}
-
-export default Username;
